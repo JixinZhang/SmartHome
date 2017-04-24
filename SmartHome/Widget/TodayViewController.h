ff2159654eb8bf313752af07572c3a9f36b48b80
JixinZhang <zzjixin@gmail.com>
2017-04-24 15:17:58 +0800

    [added] added widget

---
+++ SmartHome/Widget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  Widget
+//
+//  Created by WSCN on 24/04/2017.
+//  Copyright © 2017 JixinZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
